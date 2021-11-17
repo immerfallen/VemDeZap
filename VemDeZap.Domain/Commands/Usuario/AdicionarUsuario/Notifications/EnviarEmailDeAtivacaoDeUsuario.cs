e331cba510a4c1510f679db5949ac8956e5889b0
Maro de Melo <maromelo@tempus.digital>
2021-11-17 19:43:12 -0300

    Implementando nossas notificacoes

---
+++ VemDeZap.Domain/Commands/Usuario/AdicionarUsuario/Notifications/EnviarEmailDeAtivacaoDeUsuario.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VemDeZap.Domain.Commands.Usuario.AdicionarUsuario.Notifications
+{
+    public class EnviarEmailDeAtivacaoDeUsuario : INotificationHandler<AdicionarUsuarioNotification>
+    {
+        public async Task Handle(AdicionarUsuarioNotification notification, CancellationToken cancellationToken)
+        {
+            Debug.WriteLine("Enviar e-mail de ativacao para o usuario " + notification.Usuario.PrimeiroNome);
+        }
+    }
+}
