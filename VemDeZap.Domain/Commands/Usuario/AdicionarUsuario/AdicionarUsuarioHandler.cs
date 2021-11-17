3e98a0b93859779e22312b55c21d7d0ee1062c0b
Maro de Melo <maromelo@tempus.digital>
2021-11-17 18:39:47 -0300

    Implementando o handler através de interfaces

--- VemDeZap.Domain/Commands/Usuario/AdicionarUsuario/AdicionarUsuarioHandler.cs
+++ VemDeZap.Domain/Commands/Usuario/AdicionarUsuario/AdicionarUsuarioHandler.cs
@@ -1,17 +1,46 @@
 ﻿using MediatR;
+using prmToolkit.NotificationPattern;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using VemDeZap.Domain.Interfaces.Repositories;
 
 namespace VemDeZap.Domain.Commands.Usuario.AdicionarUsuario
 {
-    public class AdicionarUsuarioHandler : IRequestHandler<AdicionarUsuarioRequest, Response>
+    public class AdicionarUsuarioHandler : Notifiable , IRequestHandler<AdicionarUsuarioRequest, Response>
     {
-        public Task<Response> Handle(AdicionarUsuarioRequest request, CancellationToken cancellationToken)
+        private readonly IRepositoryUsuario _repositorioUsuario;
+
+        public AdicionarUsuarioHandler(IRepositoryUsuario repositorioUsuario)
         {
-            throw new NotImplementedException();
+            _repositorioUsuario = repositorioUsuario;
+        }
+
+        public async Task<Response> Handle(AdicionarUsuarioRequest request, CancellationToken cancellationToken)
+        {
+            //Validar se request veio preenchido
+            if (request == null)
+            {
+                AddNotification("Request", "Informe os dados do usuário");
+                return new Response(this);
+            }
+            // Verificar se o usuário já existe
+            if (_repositorioUsuario.Existe(x=>x.Email==request.Email))
+            {
+                AddNotification("Email", "E-mail já cadastrado no sistema");
+                return new Response(this);
+            }
+
+            Entities.Usuario usuario = new Entities.Usuario();
+
+            usuario.PrimeiroNome = request.PrimeiroNome;
+            usuario.UltimoNome = request.UltimoNome;
+            usuario.Email = request.Email;
+            usuario.Senha = request.Senha;
+
+            _repositorioUsuario.Adicionar(usuario);
         }
     }
 }
