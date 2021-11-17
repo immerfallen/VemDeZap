bbb17f7b8bf3ff9938e6e94c17e204a8f851066b
Maro de Melo <maromelo@tempus.digital>
2021-11-17 17:55:18 -0300

    Criando o command

---
+++ VemDeZap.Domain/Commands/Usuario/AdicionarUsuario/AdicionarUsuarioHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VemDeZap.Domain.Commands.Usuario.AdicionarUsuario
+{
+    public class AdicionarUsuarioHandler : IRequestHandler<AdicionarUsuarioRequest, Response>
+    {
+        public Task<Response> Handle(AdicionarUsuarioRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
